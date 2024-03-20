b2d8a5db3b2a8e1d8d143fc30fb500a1de8cb8fb
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-03-20 21:55:39 +0530

    user update requests validation errors middleware

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import {createUser, updateUser} from '../controllers/usercontroller';
+import { createUser, updateUser } from '../controllers/usercontroller';
 import { jwtCheck } from '../middlewares/auth';
+import { validateUserRequests } from '../middlewares/validation';
 const userRouter = express.Router()
 
 userRouter.post('/', jwtCheck, createUser)
 
-userRouter.put('/', jwtCheck, updateUser)
+userRouter.put('/', jwtCheck, validateUserRequests, updateUser)
 
 export default userRouter
